954d75ce909780be92ef5145cec57818712a0d2c
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2025-04-09 18:09:18 +0200

    add basic tests via Pytest

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 PYTHON = python3
 PIP = pip3
 PYTHONIOENCODING=utf8
+PYTEST_ARGS ?= "-vv"
 
 DOCKER_BASE_IMAGE = docker.io/ocrd/core:v3.3.0
 DOCKER_TAG = ocrd/docstruct
@@ -9,11 +10,20 @@ help:
 	@echo
 	@echo "  Targets"
 	@echo
-	@echo "    deps        Install only Python deps via pip"
-	@echo "    install     Install full Python package via pip"
-	@echo "    install-dev Install in editable mode"
-	@echo "    build       Build binary and source Python package"
-	@echo "    docker      Build a Docker image $(DOCKER_TAG) from $(DOCKER_BASE_IMAGE)"
+	@echo "    deps         Install only Python deps via pip"
+	@echo "    install      Install full Python package via pip"
+	@echo "    install-dev  Install in editable mode"
+	@echo "    build        Build binary and source Python package"
+	@echo "    docker       Build a Docker image $(DOCKER_TAG) from $(DOCKER_BASE_IMAGE)"
+	@echo "    test         Run tests via Pytest"
+	@echo "    repo/assets  Clone OCR-D/assets to ./repo/assets"
+	@echo "    tests/assets Copy to ./tests/assets"
+	@echo ""
+	@echo "  Variables"
+	@echo ""
+	@echo "    DOCKER_TAG  Docker container tag ($(DOCKER_TAG))"
+	@echo "    PYTEST_ARGS Additional runtime options for pytest ($(PYTEST_ARGS))"
+	@echo "                (See --help, esp. custom option --workspace)"
 
 # Install Python deps via pip
 deps:
@@ -29,6 +39,33 @@ install-dev:
 build:
 	$(PIP) install build wheel
 	$(PYTHON) -m build .
+
+# Run test
+test: tests/assets
+	$(PYTHON) -m pytest  tests --durations=0 $(PYTEST_ARGS)
+
+#
+# Assets
+#
+
+# Update OCR-D/assets submodule
+.PHONY: repos always-update tests/assets
+repo/assets: always-update
+	git submodule sync --recursive $@
+	if git submodule status --recursive $@ | grep -qv '^ '; then \
+		git submodule update --init --recursive $@ && \
+		touch $@; \
+	fi
+
+benner_herrnhuterey04_1748.ocrd.zip:
+	wget https://github.com/OCR-D/gt_structure_text/releases/download/v1.5.0/$@
+
+# Setup test assets
+tests/assets: benner_herrnhuterey04_1748.ocrd.zip
+tests/assets: repo/assets
+	mkdir -p $@
+	cp -a $</data/* $@
+	$(foreach BAG,$(filter %.zip,$^),ocrd zip spill -d $@/$(basename $(BAG)) $(BAG))
 
 docker:
 	docker build \
---
+++ tests/test_all.py
@@ -0,0 +1,72 @@
+# pylint: disable=import-error
+
+import os
+import pytest
+
+from ocrd import Workspace, run_processor
+from ocrd_models.constants import NAMESPACES as NS
+
+from docstruct.proc import OcrdDocStruct
+
+# from ocrd_pagetopdf
+def get_structure(mets):
+    metsroot = mets._tree.getroot()
+    structlink = next(metsroot.iterfind('.//mets:structLink', NS), None)
+    smlinks = {link.get('{http://www.w3.org/1999/xlink}from'):
+               link.get('{http://www.w3.org/1999/xlink}to')
+               for link in reversed(structlink.findall('./mets:smLink', NS)
+                                    if structlink else [])}
+    phymap = next(structmap for structmap in metsroot.iterfind('.//mets:structMap', NS)
+                  if structmap.get('TYPE') == 'PHYSICAL')
+    topdiv = next(phymap.iterfind('./mets:div', NS))
+    pages = {page.get('ID'): page.get('ORDER') or order
+             for order, page in enumerate(topdiv.findall('./mets:div', NS))
+             if page.get('TYPE') == "page"}
+    logmap = next((structmap for structmap in metsroot.iterfind('.//mets:structMap', NS)
+                   if structmap.get('TYPE') == 'LOGICAL'), None)
+    if logmap is None:
+        return None
+    topdiv = next(logmap.iterfind('./mets:div', NS), None)
+    if topdiv is None:
+        return None
+    # descend to deepest ADM
+    while (topdiv.get('ADMID') is None and
+           (div := topdiv.find('./mets:div', NS)) is not None):
+        topdiv = div
+    # we want to dive into multivolume_work, periodical, newspaper, year, month...
+    # we are looking for issue, volume, monograph, lecture, dossier, act, judgement, study, paper, *_thesis, report, register, file, fragment, manuscript...
+    while ((div := topdiv.find('./mets:div', NS)) is not None and
+           div.get('ADMID') is not None):
+        topdiv = div
+    #for div in topdiv.iterdescendants('{%s}div' % NS['mets']):
+    # recursive:
+    def find_depth(div, depth=0):
+        div_id = div.get('ID', div.getparent().get('ID'))
+        return {
+            'label': div.get('LABEL') or div.get('ORDERLABEL') or '',
+            'type': div.get('TYPE') or '',
+            'id': div_id,
+            'page': pages.get(smlinks.get(div_id, ''), ''),
+            'depth': depth,
+            'subs': [find_depth(subdiv, depth+1)
+                     for subdiv in div.findall('./mets:div', NS)]
+        }
+    struct = find_depth(topdiv)
+    return struct
+
+def test_docstruct(processor_kwargs):
+    ws = processor_kwargs['workspace']
+    input_file_grp = processor_kwargs['input_file_grp']
+    if not input_file_grp:
+        pytest.skip(f"workspace asset {ws.name} has no PAGE GT fileGrp")
+    offline_ws = Workspace(ws.resolver, ws.directory, mets_basename=os.path.basename(ws.mets_target))
+    structure_old = get_structure(offline_ws.mets)
+    run_processor(OcrdDocStruct,
+                  output_file_grp="", # as long as core#1321 is open, we must something here
+                  parameter=dict(mode="enmap"),
+                  **processor_kwargs,
+    )
+    ws.save_mets()
+    offline_ws.reload_mets()
+    structure_new = get_structure(offline_ws.mets)
+    assert structure_old != structure_new
